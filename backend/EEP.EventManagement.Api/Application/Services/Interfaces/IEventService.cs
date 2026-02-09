5a3b5014d01456c3647c328c920de06646808de6
Solomon <solomonf1227@gmail.com>
2026-02-09 17:15:19 +0300

    Refactor: Remove old IEventService interface

--- backend/EEP.EventManagement.Api/Application/Services/Interfaces/IEventService.cs
+++
@@ -1,9 +0,0 @@
-using EEP.EventManagement.Api.DTOs.Events;
-
-namespace EEP.EventManagement.Application.Services.Interfaces;
-
-public interface IEventService
-{
-    Task<EventResponseDto> CreateAsync(CreateEventDto dto);
-    Task<List<EventResponseDto>> GetAllAsync();
-}
