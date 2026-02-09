a23dc2399638ca168f405a3be880a50ec7a8475f
Solomon <solomonf1227@gmail.com>
2026-02-09 17:16:22 +0300

    Feat: Add NotFoundException

---
+++ backend/EEP.EventManagement.Api/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+
+namespace EEP.EventManagement.Api.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
