7eb2e9bddc8eb966fd29394dd8a023cebf73319a
Your Name <you@example.com>
2026-02-11 14:59:04 +0300

    feat: improve dashboard UI and enhance event creation screen

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,43 +1,19 @@
-<<<<<<< HEAD
- 
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { appRoutes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
     provideRouter(appRoutes),
     provideAnimations(),
     provideHttpClient(
+      withFetch(),
       withInterceptors([authInterceptor])
     )
   ]
 };
- 
-=======
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideClientHydration(withEventReplay()),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideAnimations(),
-  ],
-};
->>>>>>> f84a1d540d0e6d261c1eeb1f979fde6af1ae5672
