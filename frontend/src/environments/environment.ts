c690a76b17747f5bfc3021bfb1b0f44295c108aa
Your Name <you@example.com>
2026-03-12 17:44:13 +0300

    Update dashboard UI and global styles, adjust environment settings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000/api', 
-  apiUrl: 'http://10.27.52.182:8080',
+  apiUrl: 'http://10.27.52.119:8080',
   appName: 'EEP Event Management System',
   version: '1.0.0',
 };
